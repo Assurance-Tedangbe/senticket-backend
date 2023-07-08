dc6b49928088b1cce29530c2e96c7ef2a1b58200
tedangbe-kakiang <tedangbek@gmail.com>
2023-07-08 12:28:19 +0000

    first commit

---
+++ ManagingRestaurantTickets/src/main/java/sn/estm/managingrestauranttickets/ManagingRestaurantTicketsApplication.java
@@ -0,0 +1,13 @@
+package sn.estm.managingrestauranttickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagingRestaurantTicketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagingRestaurantTicketsApplication.class, args);
+    }
+
+}
---
+++ ManagingRestaurantTickets/src/test/java/sn/estm/managingrestauranttickets/ManagingRestaurantTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.estm.managingrestauranttickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagingRestaurantTicketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
