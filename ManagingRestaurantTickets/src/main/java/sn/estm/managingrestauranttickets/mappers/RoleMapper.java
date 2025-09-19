1e66e57cc5e43de1fbdeb70b95b1c585a1f1b8eb
tedangbe-kakiang <tedangbek@gmail.com>
2025-09-19 22:34:59 +0000

    Refactoring mapper for User & Role

--- ManagingRestaurantTickets/src/main/java/sn/estm/managingrestauranttickets/mappers/RoleMapper.java
+++ ManagingRestaurantTickets/src/main/java/sn/estm/managingrestauranttickets/mappers/RoleMapper.java
@@ -1,14 +1,25 @@
+/**
+* Generates the RoleMapper interface corresponding to the Role class
+**/
+
 package sn.estm.managingrestauranttickets.mappers;
 
 import org.mapstruct.Mapper;
 import sn.estm.managingrestauranttickets.dto.RoleDTO;
 import sn.estm.managingrestauranttickets.entities.Role;
 
-@Mapper(componentModel = "spring")
+/**
+ * Mapper interface for converting between {@link Role} entities and {@link RoleDTO} data transfer objects.
+ * This interface uses MapStruct to automatically generate the implementation at build time.
+ * The {@code componentModel = "spring"} parameter allows the generated mapper to be injected as a Spring bean.
+
+ * {@link #toRoleDTO(Role)}: Converts a {@link Role} entity to a {@link RoleDTO}
+ * {@link #toRole(RoleDTO)}: Converts a {@link RoleDTO} to a {@link Role} entity
+ */
+@Mapper(componentModel = "spring", uses = UserMapper.class)
 public interface RoleMapper {
 
     RoleDTO toRoleDTO(Role role);
 
     Role toRole(RoleDTO roleDto);
-
 }
--- ManagingRestaurantTickets/src/main/java/sn/estm/managingrestauranttickets/mappers/UserMapper.java
+++ ManagingRestaurantTickets/src/main/java/sn/estm/managingrestauranttickets/mappers/UserMapper.java
@@ -1,14 +1,30 @@
+/**
+* Generates the UserMapper interface corresponding to the User class
+**/
+
 package sn.estm.managingrestauranttickets.mappers;
 
 import org.mapstruct.Mapper;
+
 import sn.estm.managingrestauranttickets.dto.UserDTO;
 import sn.estm.managingrestauranttickets.entities.User;
 
-@Mapper(componentModel = "spring")
-public interface UserMapper {
+/**
+ * Mapper interface for converting between {@link User} entities and {@link UserDTO} data transfer objects.
+ * This interface uses MapStruct to automatically generate the implementation at build time.
+ * The {@code componentModel = "spring"} parameter allows the generated mapper to be injected as a Spring bean.
 
+ * {@link #toUserDTO(User)}: Converts a {@link User} entity to a {@link UserDTO}
+ * {@link #toUser(UserDTO)}: Converts a {@link UserDTO} to a {@link User} entity
+ */
+@Mapper(componentModel = "spring", uses = RoleMapper.class)
+public interface UserMapper {
     UserDTO toUserDTO(User user);
 
     User toUser(UserDTO userDTO);
-
 }
+
+
+
+
+
