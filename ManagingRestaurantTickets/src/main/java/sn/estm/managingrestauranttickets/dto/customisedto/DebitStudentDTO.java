bc24408a5aab5275ddec8f72364cf6583d0c961a
tedangbe-kakiang <tedangbek@gmail.com>
2026-01-28 11:13:04 +0000

    Modification of debitAccount in ticketService

---
+++ ManagingRestaurantTickets/src/main/java/sn/estm/managingrestauranttickets/dto/customisedto/DebitStudentDTO.java
@@ -0,0 +1,22 @@
+package sn.estm.managingrestauranttickets.dto.customisedto;
+
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
+import jakarta.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DebitStudentDTO {
+
+    @NotNull(message = "User ID is required")
+    private Long debitStudentId;
+
+    @NotBlank(message = "Username is required")
+    private String username;
+}
